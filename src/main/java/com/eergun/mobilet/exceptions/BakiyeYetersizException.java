6378cf8b4f11449372d8436001bb12a02aa39ce4
msacak <msacakk@gmail.com>
2024-10-14 13:49:31 +0300

    Exception handlerlar oluşuturlmaya başlandı.

--- src/main/java/com/eergun/mobilet/exceptions/BakiyeYetersizException.java
+++
@@ -1,8 +0,0 @@
-package com.eergun.mobilet.exceptions;
-
-public class BakiyeYetersizException extends RuntimeException{
-    public BakiyeYetersizException(){
-        super("BAKİYE YETERSİZ!!!!!"); //bakiyeniz ytersiz diyen ablanın sesi koyulacak.
-    }
-
-}
--- src/main/java/com/eergun/mobilet/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.eergun.mobilet.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(){
-        super("User not found!!!!!!");
-    }
-}
